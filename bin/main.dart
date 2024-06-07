11836bcdec909f18433fe166d5b15d4b78e0beaa
AnhQuan Nguyen <j4qfrost@gmail.com>
2024-06-07 16:34:32 -0500

    logger

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,12 @@
 import 'package:conduit_app_test/conduit_app_test.dart';
 
 Future main() async {
+  final logger = Logger('conduit');
   const hostname = String.fromEnvironment('HOSTNAME');
   print('Resolving hostname $hostname');
   final addresses =
       await InternetAddress.lookup(hostname, type: InternetAddressType.IPv4);
-  print('Hostname resolves to $addresses');
+  logger.info('Hostname resolves to $addresses');
   final app = Application<ConduitAppTestChannel>()
     ..options.address = addresses.first
     ..options.configurationFilePath = "config.yaml"
