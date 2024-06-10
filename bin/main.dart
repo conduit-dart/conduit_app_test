053a79fe5a557ebc322170f5d3853bde5985a487
AnhQuan Nguyen <j4qfrost@gmail.com>
2024-06-09 22:25:30 -0500

    dns lookup wait

--- bin/main.dart
+++ bin/main.dart
@@ -1,8 +1,12 @@
 import 'package:conduit_app_test/conduit_app_test.dart';
 
 Future main() async {
+  const hostname = String.fromEnvironment('HOSTNAME');
+
+  final addresses =
+      await InternetAddress.lookup(hostname, type: InternetAddressType.IPv4);
   final app = Application<ConduitAppTestChannel>()
-    ..options.address = InternetAddress.anyIPv4
+    ..options.address = addresses.first
     ..options.configurationFilePath = "config.yaml"
     ..options.port = 8888;
 
