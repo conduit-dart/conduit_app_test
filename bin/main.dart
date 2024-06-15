ec7c43824da2f8f125416c022fc9723511003f45
AnhQuan Nguyen <j4qfrost@gmail.com>
2024-06-14 18:28:05 -0700

    rigorous hostname check

--- bin/main.dart
+++ bin/main.dart
@@ -2,15 +2,14 @@ import 'package:conduit_app_test/conduit_app_test.dart';
 
 Future main() async {
   const hostname = String.fromEnvironment('HOSTNAME');
-
-  final addresses = await InternetAddress.lookup(hostname);
+  print('Resolving hostname $hostname');
+  final addresses =
+      await InternetAddress.lookup(hostname, type: InternetAddressType.IPv4);
+  print('Hostname resolves to $addresses');
   final app = Application<ConduitAppTestChannel>()
     ..options.address = addresses.first
     ..options.configurationFilePath = "config.yaml"
     ..options.port = 8888;
 
   await app.startOnCurrentIsolate();
-
-  print("Application started on port: ${app.options.port}.");
-  print("Use Ctrl-C (SIGINT) to stop running the application.");
 }
